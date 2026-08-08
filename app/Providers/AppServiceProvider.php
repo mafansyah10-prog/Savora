65115429ff2195680bf3a244582b4de0b62f0f61
mafansyah10-prog <mafansyah10-prog@users.noreply.github.com>
2026-08-08 13:52:59 +0700

    Override mail and queue configurations dynamically on production server

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Force SMTP and Sync queue on public server
+        if (app()->environment('production') || (isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST'] === 'savorakuliner.my.id')) {
+            config([
+                'mail.default' => 'smtp',
+                'mail.mailers.smtp.transport' => 'smtp',
+                'mail.mailers.smtp.host' => 'smtp.gmail.com',
+                'mail.mailers.smtp.port' => 465,
+                'mail.mailers.smtp.encryption' => 'ssl',
+                'mail.mailers.smtp.scheme' => 'smtps',
+                'mail.mailers.smtp.username' => 'm.afansyah10@gmail.com',
+                'mail.mailers.smtp.password' => 'hfyozosdkveaqywb',
+                'mail.from.address' => 'm.afansyah10@gmail.com',
+                'mail.from.name' => 'Savora',
+                'queue.default' => 'sync',
+            ]);
+        }
     }
 }
